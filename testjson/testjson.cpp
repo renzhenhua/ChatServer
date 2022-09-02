3faf422a5f6a458ea807dd13961f695895d37eda
renzhenhua <2452503780@qq.com>
2022-09-02 09:27:30 +0800

    测试json.hpp使用

---
+++ testjson/testjson.cpp
@@ -0,0 +1,73 @@
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string func1() {
+    json js;
+    js["msg_type"] = 2;
+    js["from"] = "zhang san";
+    js["to"] = "li si";
+    js["msg"] = "hello, what are you doing now?";
+
+    string sendBuf = js.dump();
+
+    return sendBuf;
+}
+
+string func2() {
+    json js;                     // 添加数组
+    js["id"] = {1, 2, 3, 4, 5};  // 添加key-value
+    js["name"] = "zhang san";    // 添加对象
+    js["msg"]["zhang san"] = "hello world";
+    js["msg"]["liu shuo"] =
+        "hello china";  // 上面等同于下面这句一次性添加数组对象
+    js["msg"] = {{"zhang san", "hello world"}, {"liu shuo", "hello china"}};
+    // cout << js << endl;
+
+    string sendBuf = js.dump();
+    return sendBuf;
+}
+
+string func3() {
+    json js;
+    // 直接序列化一个vector容器
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(5);
+    js["list"] = vec;
+    // 直接序列化一个map容器
+    map<int, string> m;
+    m.insert({1, "黄山"});
+    m.insert({2, "华山"});
+    m.insert({3, "泰山"});
+    js["path"] = m;
+
+    string sendBuf = js.dump();
+    return sendBuf;
+}
+
+int main() {
+    string recvBuf = func3();
+
+    json jsBuf = json::parse(recvBuf);
+
+    vector<int> vec = jsBuf["list"];
+
+    for (int &v : vec) cout << v << endl;
+
+    map<int, string> mymap = jsBuf["path"];
+
+    for (auto &p : mymap) {
+        cout << p.first << " " << p.second << endl;
+    }
+
+    return 0;
+}
